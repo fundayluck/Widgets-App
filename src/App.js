769889fd15c523931f2f733865cdc007a5b5678e
jember08 <afandayul@gmail.com>
2021-10-10 12:31:49 +0700

    Text input with Hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from "react";
-import Accordion from "./components/Accordion";
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-const items = [
-  {
-    title: "Apa itu React?",
-    content: "React adalah Front End Javascript frameWork",
-  },
-  {
-    title: "kenapa menggunakan React?",
-    content: "karena saya senang menggunakan framework ini.",
-  },
-  {
-    title: "bagaimana cara menggunakan react?",
-    content: "menggunakan react dengan membuat component",
-  },
-];
+// const items = [
+//   {
+//     title: "Apa itu React?",
+//     content: "React adalah Front End Javascript frameWork",
+//   },
+//   {
+//     title: "kenapa menggunakan React?",
+//     content: "karena saya senang menggunakan framework ini.",
+//   },
+//   {
+//     title: "bagaimana cara menggunakan react?",
+//     content: "menggunakan react dengan membuat component",
+//   },
+// ];
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
