cde65a0d130d6ed0148beb6d602afef2df6fcb92
jember08 <afandayul@gmail.com>
2021-10-10 09:49:01 +0700

    creating file Accordion.js and refactoring file App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "Apa itu React?",
+    content: "React adalah Front End Javascript frameWork",
+  },
+  {
+    title: "kenapa menggunakan React?",
+    content: "karena saya senang menggunakan framework ini.",
+  },
+  {
+    title: "bagaimana cara menggunakan react?",
+    content: "menggunakan react dengan membuat component",
+  },
+];
 
 export default () => {
-  return <h1>Widget App</h1>;
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
