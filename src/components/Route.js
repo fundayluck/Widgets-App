e7f1e464b40ab80b1dfed37275fee22d3d227a2a
jember08 <afandayul@gmail.com>
2021-10-17 02:58:42 +0700

    Detecting Navigation

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,6 +1,18 @@
-import React from "react";
+import { useEffect } from "react";
 
 const Route = ({ path, children }) => {
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log("location Change");
+    };
+
+    window.addEventListener("popstate", onLocationChange);
+
+    return () => {
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []);
+
   return window.location.pathname === path ? children : null;
 };
 
